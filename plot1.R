54f813f92a0b2c502e9249557338701a74284fe1
saberpv-eco <sbr.pahlavan@gmail.com>
2021-04-25 13:04:04 +0400

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,24 @@
+# create a temporary file on the local disk
+tf <- tempfile()
+# download data to the temporary file
+download.file( "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip" , tf , mode = 'wb' )
+
+# unzip the contents 
+data <- unzip( tf , exdir = tempdir() )
+
+NEI <- readRDS(file = "summarySCC_PM25.rds")
+SCC <- readRDS(file = "Source_Classification_Code.rds")
+
+TotalNEI<-with(NEI, tapply(Emissions , year , FUN = sum))
+
+png(filename='plot1.png')
+barplot(TotalNEI
+        , names = names(TotalNEI)
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
+dev.off()
+
+
+
+
+
